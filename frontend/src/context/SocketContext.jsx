5d5b21027bc586a558d5d0dca03859ee22f6894c
sonngok4 <ngoson2k40501@gmail.com>
2025-02-06 15:32:02 +0700

    change domain for socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("https://chat-app-yt-eic6.onrender.com/", {
+            const socket = io("https://chat-app-yt-eic6.onrender.com", {
                 query: {
                     userId: authUser._id,
                 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			'/api': {
-				target: 'https://chat-app-yt-eic6.onrender.com/',
+				target: 'https://chat-app-yt-eic6.onrender.com',
 				// changeOrigin: true,
 				// secure: false,
 				// rewrite: path => path.replace(/^\/api/, '')
