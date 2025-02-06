2a6e8dd9ee9988289f20e2df3d2b3e637ac034b8
sonngok4 <ngoson2k40501@gmail.com>
2025-02-06 15:11:45 +0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
-import { app, server } from './socket/socket.js';
+import path from 'path';
 import connectDB from './configs/database.config.js';
-import loggerMiddleware from './middlewares/loggerMiddleware.js';
+import { app, server } from './socket/socket.js';
 dotenv.config();
-
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,21 +20,18 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
-app.get('/', (req, res) => {
-	// root route http://localhost:5000/
-	res.send('Hello World!');
-});
-
-app.get('/api/', (req, res) => {
-	// root route http://localhost:5000/api/
-	res.send('Hello World!');
-});
 
 app.use('/api/auth', authRoutes);
 
 app.use('/api/messages', messageRoutes);
 
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
 	// Connect to MongoDB
